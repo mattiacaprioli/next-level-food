23e45ff08f5537bc1be5c2a3572d9d1ec32a7709
Mattia Caprioli <capriolimattia1994@gmail.com>
2024-04-02 09:08:54 +0200

    feat: error message

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,12 @@
+'use client'
+import {useFormState} from 'react-dom';
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, {message: null})
 
   return (
     <>
@@ -42,8 +46,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label='Your image' name='image' />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
